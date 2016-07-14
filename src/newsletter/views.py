e38503dbc2d018f996fe0ba92fb7b57d95f97008
Adnan Ahmad <adnanahmad07@gmail.com>
2016-07-14 12:36:30 +0530

    Updates views

--- src/newsletter/views.py
+++ src/newsletter/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
-
+from .forms import SignUpForm
 # Create your views here.
 
 
 def home(request):
-	return render(request,"home.html",{})
-	
+	title="This is a title"
+	form=SignUpForm(request.POST or None)
+
+	if form.is_valid():
+		instance=form.save(commit=False)
+		instance.save()
+		print (instance)
+		
+
+
+	context={
+	"template_title":title,
+	"form":form
+	}
+
+	return render(request,"home.html",context)
+
